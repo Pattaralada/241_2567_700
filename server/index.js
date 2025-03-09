1b8daf790dd70354afeedea9c0f94a093c7909a8
Pattaralada <pattaralada.da@ku.th>
2025-03-09 23:23:35 +0700

    Message Errors

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const app = express();
 
 const port = 8000;
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -22,59 +21,8 @@ const initMySQL = async () => {
     })
 }
 
-/*
-app.get('/testdb', (req, res) => {
-    mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'webdb',
-        port: 8830
-    }).then((conn) => {
-        conn
-        .query('SELECT * FROM users')
-        .then((results) => {
-            res.json(results[0])
-        })
-        .catch((error) => {
-            console.log('error', error.message)
-            res.status(500).json({error: 'ERROR fetching users'})
-        })
-    })
-})
-*/
-
-/*
-app.get('/testdbnew', async (req, res) => {
-    try {
-        const results = await conn.query('SELECT * FROM users')
-        res.json(results[0])
-    } catch (error) {
-        console.log('error', error.message)
-        res.status(500).json({error: 'ERROR fetching users'})
-    }       
-})
-*/
-
-/*
-GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
-POST /users สำหรับสร้าง users ใหม่บันทึกเข้าไป
-GET /users/:id สำหรับดึง users รายคนออกมา
-PUT /users/:id สำหรับแก้ไข user รายคนตาม id ที่ต้องการ
-DELETE /users/:id สำหรับลบ user รายคยตาม id ที่ต้องการ
-*/
-
 // path = GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
 app.get('/users', async (req, res) => {
-    /*const filterUsers = users.map(user => {
-        return {
-            firstname: user.firstname,
-            lastname: user.lastname,
-            fullname: user.firstname + ' ' + user.lastname
-        }
-    })
-    res.json(filterUsers)
-    */
     const results = await conn.query('SELECT * FROM users')
     res.json(results[0])
 })
@@ -84,65 +32,106 @@ app.post('/users', async (req, res) => {
 
     try {
         let user = req.body;
+        const errors = validateData(user)
+        if (errors.length > 0) {
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
         const results = await conn.query('INSERT INTO users SET ?', user)
         res.json({
             message: 'Create user successfully',
             data: results[0]
         })
+
     } catch (error) {
-        console.log('error', error.message)
+        const errorMessage = error.message || 'something went wrong'
+        const errors = error.errors || []
+        console.error('error message: ', error.message)
         res.status(500).json({
-            message: 'Something went wrong',
-            errorMessage: error.message
+            message: error.message,
+            errors: errors
         })
     }
 })
 
-// path = GET /users สำหรับ get users ทั้งหมดที่บันทึกไว้
+const validateData = (userData) => {
+    let errors = [];
+    if (!userData.firstName) {
+        errors.push('กรุณากรอกชื่อ');
+    }
+    if (!userData.lastName) {
+        errors.push('กรุณากรอกนามสกุล');
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ');
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ');
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกงานอดิเรก');
+    }
+    return errors;
+};
+
+// path = GET /users/:id สำหรับดึง users รายคนออกมา
 app.get('/users/:id', async (req, res) => {
     try {
         let id = req.params.id;
         const results = await conn.query('SELECT * FROM users WHERE id = ?', id)
+
         if (results[0].length == 0) {
-            throw { statusCode: 404, message: 'User not found' }
+            throw { stsatusCode: 404, message: 'user not found' }
+
         }
         res.json(results[0][0])
+
     } catch (error) {
-        console.log('error', error.message)
+        console.error('error: ', error.message)
+        let stsatusCode = error.stsatusCode || 500
         res.status(500).json({
-            message: 'Something went wrong',
+            message: 'something went wrong',
             errorMessage: error.message
         })
     }
+
 })
 
-// path = GET /users/:id สำหรับดึง users รายคนออกมา
+
+//path: PUT /users/:id สำหรับแก้ไข users รายคน (ตาม id ที่บันทึกเข้าไป)
 app.put('/users/:id', async (req, res) => {
-    try{
-       let id = req.params.id;
-       let updateUser = req.body;
-       const results = await conn.query(
-           'UPDATE users SET ? WHERE id = ?', 
-           [updateUser, id]    
-       )
-       res.json({
-           message: 'Update user successfully',
-           data: results[0]
-       })
-   }catch(error){
-       console.error('error: ', error.message)
-       res.status(500).json({
-           message: 'something went wrong',
-           errorMessage: error.message
-       })
-   }  
+
+    try {
+        let id = req.params.id;
+        let updateUser = req.body;
+        const results = await conn.query(
+            'UPDATE users SET ? WHERE id = ?',
+            [updateUser, id]
+        )
+
+        res.json({
+            message: 'Update user successfully',
+            data: results[0]
+        })
+
+    } catch (error) {
+        console.error('error: ', error.message)
+        res.status(500).json({
+            message: 'something went wrong',
+            errorMessage: error.message
+        })
+    }
+
+
 })
 
-//path = DELETE /users/:id สำหรับลบ user รายคยตาม id ที่ต้องการ
+//path: DELETE /users/:id สำหรับลบ users รายคน (ตาม id ที่บันทึกเข้าไป)
 app.delete('/users/:id', async (req, res) => {
     try {
         let id = req.params.id;
-        const results = await conn.query('DELETE from users WHERE id = ?', id)    
+        const results = await conn.query('DELETE from users WHERE id = ?', id)
         res.json({
             message: 'Delete user successfully',
             data: results[0]
@@ -154,9 +143,10 @@ app.delete('/users/:id', async (req, res) => {
             errorMessage: error.message
         })
     }
-    
+
 })
+
 app.listen(port, async (req, res) => {
     await initMySQL()
-    console.log('Http Server is running on' + port);
+    console.log('Http Server is running on port' + port)
 });
